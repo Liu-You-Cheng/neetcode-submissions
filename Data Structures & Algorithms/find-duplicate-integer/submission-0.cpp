db663025f365d962de98d0ae9c03d2dfa96c297c
劉又誠 <76584855+Liu-You-Cheng@users.noreply.github.com>
2026-07-06 19:03:23 +0800

    Add: find-duplicate-integer - submission-0

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-0.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int s = nums[0], f = nums[nums[0]];
+        // cycle
+        while(s != f){
+            s = nums[s];
+            f = nums[nums[f]];
+        }
+
+        s = 0;
+
+        while(s != f){
+            s = nums[s];
+            f = nums[f];
+        }
+        return s;
+    }
+};
